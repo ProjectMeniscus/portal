87f48410f45ce199f2cb85a3bbf9fed1d9cf718e
John Hopper <john.hopper@rackspace.com>
2013-10-23 13:57:11 -0500

    cleanup and addition of SSL configurations

--- portal/tests/server_test_example.py
+++ portal/tests/server_test_example.py
@@ -1,13 +1,23 @@
+import signal
 import simplejson as json
 
-from multiprocessing import Process
+import portal.config as config
 
-from portal.log import get_logger
-from portal.server import start_io, SyslogServer
+from portal.log import get_logger, get_log_manager
+from portal.server import SyslogServer, start_io, stop_io
 from portal.input.syslog import SyslogMessageHandler
 
+config = config.load_config()
 
-_LOG = get_logger('portal.tests.server_test')
+logging_manager = get_log_manager()
+logging_manager.configure(config)
+
+_LOG = get_logger(__name__)
+
+
+def stop(signum, frame):
+    _LOG.debug('Stop called at frame:\n{}'.format(frame))
+    stop_io()
 
 
 class MessageHandler(SyslogMessageHandler):
@@ -24,15 +34,39 @@ class MessageHandler(SyslogMessageHandler):
     def on_msg_part(self, message_part):
         self.msg.extend(message_part)
 
-    def on_msg_complete(self):
+    def on_msg_complete(self, message_length):
         message_dict = self.msg_head.as_dict()
+        message_dict['message_length'] = str(message_length)
         message_dict['message'] = str(self.msg)
         _LOG.debug('Message: {}'.format(json.dumps(message_dict)))
         self.msg_head = None
         del self.msg[:]
 
 
-if __name__ == "__main__":
-    syslog_server = SyslogServer(("127.0.0.1", 5140), MessageHandler())
+if __name__ == '__main__':
+    ssl_options = None
+
+    cert_file = config.ssl.cert_file
+    key_file = config.ssl.key_file
+
+    if None not in (cert_file, key_file):
+        ssl_options = dict()
+        ssl_options['certfile'] = cert_file
+        ssl_options['keyfile'] = key_file
+
+        _LOG.debug('SSL enabled: {}'.format(ssl_options))
+
+    # Set up the syslog server
+    syslog_server = SyslogServer(
+        config.core.syslog_bind_host,
+        MessageHandler(),
+        ssl_options)
     syslog_server.start()
+
+    # Take over SIGTERM and SIGINT
+    signal.signal(signal.SIGTERM, stop)
+    signal.signal(signal.SIGINT, stop)
+
+    # Start I/O
     start_io()
+
